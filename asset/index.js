4d77867447fc2789bb153370e791c5a862b19e40
wurui <wurui.wr@gmail.com>
2017-12-15 18:13:01 +0800

    firstcommit

--- asset/index.js
+++ asset/index.js
@@ -1,7 +1,52 @@
-define([],function(){
+define(['oxjs'],function(OXJS){
   return {
     init:function($mod){
+    	/**
+		如何传值：
+		1.　window.opener　回调；在移动端，特别是在各客户端中交互的兼容性不好
+		2.　a 链接过来，读写localStorage，设置key是个问题
+		3.　a 链接过来，读写都走服务端（最好）
+    	*/
+    	/*
+    	var storeKey='current_city',
+    	city=localStorage.getItem(storeKey);
+    	if(city){
+    		$('[value="'+city+'"]',$mod).focus()
+    	}*/
+    	//此处应该是调用接口去修改服务端数据，然后history.back
+    	$mod.on('click','.item',function(e){
+    		//console.log('fuck',e.target);
+    		$(e.target).addClass('selected').siblings().removeClass('selected')
+    		//localStorage.setItem(storeKey,e.target.value)
+    		/*
+    		window.opener && window.opener.callback && window.opener.callback(e.target.value);
+    		window.close();
+    		*/
+    		//history.back();
+            /*
+    		OXJS.useModel && OXJS.useModel($mod).put({
+    			address:{
+    				city:e.target.value
+    			}
+    		})
 
+    		OXJS.useREST('address').put({city:e.target.value},function(){
+    			if(document.referrer){
+    				history.back();
+    			}
+    			
+    		})
+            */
+            $mod.OXPut({
+                address:{
+                    city:e.target.value
+                }
+            },function(r){
+                if(document.referrer){
+                    history.back();
+                }
+            })
+    	})
     }
   }
 })
--- mod.xsl
+++ mod.xsl
@@ -2,9 +2,20 @@
     <xsl:template match="/root" name="wurui.ctrip-hot-cities">
         <!-- className 'J_OXMod' required  -->
         <div class="J_OXMod oxmod-ctrip-hot-cities" ox-mod="ctrip-hot-cities">
-            <h1>
-                This is mod ctrip-hot-cities;
-            </h1>
+            <h3>
+                热门城市
+            </h3>
+            <div class="list">
+            	<xsl:variable select="data/address/city" name="city"/>
+            	<xsl:for-each select="data/string-list/i">
+
+            		<input type="button" class="item" value="{.}">
+            			<xsl:if test=". = $city">
+            				<xsl:attribute name="class">item selected</xsl:attribute>
+            			</xsl:if>
+            		</input>
+            	</xsl:for-each>
+            </div>
         </div>
     </xsl:template>
 </xsl:stylesheet>
